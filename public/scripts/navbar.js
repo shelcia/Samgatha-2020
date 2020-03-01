f21a1a4cf27664837d0102751e13cad7cf1cf58e
Aditya D.S <adithya18062000@gmail.com>
2020-03-02 04:12:35 +0530

    home page final

---
+++ public/scripts/navbar.js
@@ -0,0 +1,8 @@
+const nav = document.querySelector('nav');
+const hamburger = document.querySelector('span.navbar-toggler-icon');
+
+hamburger.addEventListener('click', function (e) {
+    e.preventDefault();
+    nav.classList.toggle('bg-transparent');
+    nav.classList.toggle('bg-violent-violet');
+});
